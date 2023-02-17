8d516e40485728f39245e1604ee85d23c835839e
Conan Duval-Manahan <conanduval-manahan@outlook.com>
2023-02-17 23:15:43 +0000

    removing seed as not necessary for now

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,3 @@
 require "open-uri"
 
-file = URI.open("https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/NES-Console-Set.jpg/1200px-NES-Console-Set.jpg")
-Movie = Movie.new(title: "NES", body: "A great console")
-Movie.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
-Movie.save
+file = URI.open
